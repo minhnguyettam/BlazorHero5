8c49985d418999709273938b3ed0d5cb2665a6bb
minhnguyettam <minhnguyettam20781@gmail.com>
2021-03-27 14:11:02 +0700

    Initial commit

---
+++ BlazorHero.CleanArchitecture/Server/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using BlazorHero.CleanArchitecture.Application.Models.Chat;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(ChatHistory chatHistory, string userName)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", chatHistory, userName);
+        }
+    }
+}
